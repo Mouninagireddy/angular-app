dc2192cbc96b1ff8e363f52a781142234bcdec55
Mouninagireddy <mouninagireddy554@gmail.com>
2020-12-13 17:36:02 -0600

    get data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,33 +9,47 @@ import { AppService } from './app.service';
 export class AppComponent {
 
   appType: string = ""; //intially it's a empty string 
-  //when we call this changeToReact() fun empty str replaces with react app same with angular fun
+       //when we call this changeToReact() fun empty str replaces with react app same with angular fun
+  
+  studentResponse : any; 
+
+  employeeResponse: any; //response can be anything in object
+
+  showTable : boolean = false;
 
   constructor(private appName:AppService) //in constructor giving the address of a service file to import into this file 
                    //AppService is a cls name of service file, appName is just a name which stores the AppService cls
   {
-
-  };
-
-  searchByCaseNumber()
-  {
-    console.log("search by case number button worked"); //to check in o/p in console 
   };
 
   getAllEmployees(){
     console.log("ts file");
     //write a code to call a fun in service file
-   this.appName.getEmployees();
-
+    this.appName.getEmployees().subscribe((resp)=>{
+     console.log(resp);
+     this.employeeResponse= resp; //to remove the status values in object we are access just accessing data values 
+     this.showTable = true;
+    });
   };
 
   changeToReact(){
     console.log("This is React");
-    this.appType="react app";
+    this.appType="This is React app";
   }
+
   changeToAngular(){
     console.log("This is Angular");
-    this.appType="angular app";
+    this.appType="This is Angular app";
+  };
+  
+  getAllStudents(){
+    console.log("Student details")
+    this.appName.getStudentDetails().subscribe( (resp) =>{
+      console.log(resp);
+      
+     this.studentResponse = resp;
+     console.log(this.studentResponse);
+    });
   };
 
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
   getEmployees(){
     //when we call this fun from the ts file we can c the o/p in console
     console.log("Service File");
+      //request going to backend
+    return this.http.get("http://dummy.restapiexample.com/api/v1/employees")
   }
+
+  getStudentDetails(){
+    console.log("student info");
+    return this.http.get("http://dummy.restapiexample.com/api/v1/employees")
+  };
+
 }
